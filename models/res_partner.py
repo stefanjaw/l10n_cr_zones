aedcb91f1706a376c52e778b82ed2b31c538e1ed
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2020-07-22 15:45:42 -0600

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,9 +5,14 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    canton_id = fields.Many2one("res.country.canton", string="Canton")
-    distrito_id = fields.Many2one("res.country.distrito", string="Distrito")
-    barrio_id = fields.Many2one("res.country.barrio", string="Barrio")
-    
     country_id_code = fields.Char(string='Country Code',related='country_id.code', store=True)
+
+    canton_id = fields.Many2one("res.country.canton", string="Canton")
+    canton_id_comments = fields.Char(related='canton_id.comments')
+
+    distrito_id = fields.Many2one("res.country.distrito", string="Distrito")
+    distrito_id_comments = fields.Char(related='distrito_id.comments')
+
+    barrio_id = fields.Many2one("res.country.barrio", string="Barrio")
+    barrio_id_comments = fields.Char(related='barrio_id.comments')
     
