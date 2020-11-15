1b8e9a10f5656c2c5b7bec542183ac3b9be48c49
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2020-11-15 11:24:01 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Localization',
+    'category': 'Accounting',
     'version': '13.0.0.4',
 
     # any module necessary for this one to work correctly
