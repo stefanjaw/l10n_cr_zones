6f790811350f539493909ec0ac2449e8f1f0268b
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2023-12-28 17:54:24 -0600

    Update

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting',
-    'version': '13.0.0.4',
+    'version': '0.0.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'contacts'],
